0dccd3401886996b7dfb8637ee169aa0ac4a099d
KALIIKARDZ <145854310+KALIIKARDZ@users.noreply.github.com>
2025-03-23 16:14:34 +0300

    Update task_one.cs

--- task_one.cs
+++ task_one.cs
@@ -1,18 +1,21 @@
 using System;
 
 class LuckyGame
-{
+{ 
     static void Main()
     {
-        int secretNumber = 7; 
         int totalScore = 0;
+        Random rand = new Random(); // Choose to set  secret num as random variable,keeps it interesting its a game after all 
+        int secretNumber = rand.Next(2, 10); // Generates a random secret number between 2 and 9
 
-        Console.Write("Enter number of rounds: ");
+        Console.WriteLine($"(Secret number has been randomly chosen!)");
+
+        Console.Write("Enter the number of rounds: ");
         int rounds = int.Parse(Console.ReadLine());
 
-        for (int i = 0; i < rounds; i++)
+        for (int i = 1; i <= rounds; i++)
         {
-            Console.Write("Enter your lucky number: ");
+            Console.Write($"Enter your lucky number for round {i} Champ !!: ");
             int luckyNumber = int.Parse(Console.ReadLine());
 
             int remainder = luckyNumber % secretNumber;
@@ -20,24 +23,26 @@ class LuckyGame
             if (remainder == 0)
             {
                 totalScore += 1; // Draw
+                Console.WriteLine("Draw! +1 point, one on one my guy niko na wewe HAHAHA..");
             }
-            else if (remainder % 2 == 0)
+            else if (remainder % 2 == 0) // Being divisble by 2 without reminder makes it even
             {
                 totalScore += 3; // Win
+                Console.WriteLine("Win! +3 points");
             }
             else
             {
                 totalScore -= 3; // Loss
+                Console.WriteLine("Loss! -3 points, Don't cry on my keyboard!!");
             }
         }
 
+        Console.WriteLine($"\nFinal Score: {totalScore}");
+        Console.WriteLine($"(The secret number was: {secretNumber})"); // Revealing the secret number at the end
+
         if (totalScore > 0)
-        {
-            Console.WriteLine("Congratulations! You won with a score of " + totalScore);
-        }
+            Console.WriteLine("Congratulations! You win! , You must really be lucky!");
         else
-        {
-            Console.WriteLine("You lost. Your final score is " + totalScore);
-        }
+            Console.WriteLine("Game over! You lose. Give me 50 Pushups!!");
     }
 }
